915ec3394f1a93dd4f78ea793a85f9b837dace62
Micaias Carvalho Vieira <73481473+micaiasdev@users.noreply.github.com>
2025-10-01 20:37:36 -0300

    modify_node

--- node.py
+++ node.py
@@ -1,7 +1,16 @@
 class Node: 
-  def __init__(self, state, action, parent, cost):
+  def __init__(self, state, action, parent):
     self.state = state
     self.action = action
     self.parent = parent
-    self.cost = cost
+    if parent is not None:
+      self.cost = parent.cost + 1
+    else:
+      self.cost = 0
     
+    
+if __name__ == "__main__":
+  raiz = Node([1,2,3,0,4,5,6,7,8], None, None)
+  filho = Node([1,2,3,4,0,5,6,7,8], "right", raiz)
+   
+  print(filho.parent.cost)
