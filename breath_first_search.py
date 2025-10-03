ceebea4ac754c0f4c46cb5bbc0927892588841dd
Micaias Carvalho Vieira <73481473+micaiasdev@users.noreply.github.com>
2025-10-03 10:11:42 -0300

    modify_func_bfs

--- breath_first_search.py
+++ breath_first_search.py
@@ -1,6 +1,6 @@
 from typing import List, Dict, Tuple, Set
 from collections import deque
-from generate_succeessors import create_successors, print_board
+from generate_succeessors import create_successors
 from Node import Node
 
 init_board = [
@@ -24,7 +24,6 @@ def bfs(node: Node):
   explorados = set()
   while(1):
     actual_level_nodes = list()
-    actual_level = 0
     while fronteira:
       actual_node = fronteira.popleft()
       if actual_node in explorados:
@@ -32,12 +31,11 @@ def bfs(node: Node):
       if actual_node.state == final_state:
         return actual_node
       actual_level_nodes.append(actual_node)
-      
-    explorados.add(tuple(actual_level_nodes))
-    for explo_node in explorados[actual_level]:
-      new_states 
-      
-      
+      explorados.add(actual_node)
+    for explo_node in actual_level_nodes:
+       expand_nodes = create_successors(explo_node)
+       fronteira.append(expand_nodes)
+
 if __name__ == "__main__":
   bfs(root)
   
